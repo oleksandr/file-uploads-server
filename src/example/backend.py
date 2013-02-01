255fb5176841194926e03548e582cc80d42b0ba8
Oleksandr Lobunets <alexander.lobunets@gmail.com>
2013-02-01 13:15:09 +0100

    Switched to gunicorn-based backend. Removed demo code from the flask app

--- src/example/backend.py
+++ src/example/backend.py
@@ -1,20 +1,14 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 app.debug = False
 
-
-@app.route('/')
-def hello_world():
-    return """<h1>Example File Uploads Handling Backend</h1>
-This is just for demo purposes."""
-
-@app.route('/form', methods=['GET'])
-def demo_form():
-    return render_template('form.html'), 200
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    """
+    This is the place where you iterate through uploaded files,
+    optionally process and relocate them (in either sync or async way).
+    """
     app.logger.debug("upload_file() INVOKED. We have: request.files = %r" % request.files)
     return jsonify(status=u'OK', msg=u'This is just an example response.', fields=request.form)
 
